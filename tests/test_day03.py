1c253bc3421a897715f9a316b076efbcbd204dca
Holocron <jedi.holocron@gmail.com>
2024-12-02 21:10:18 -0600

    stage day 3

---
+++ tests/test_day03.py
@@ -0,0 +1,17 @@
+from aoc2024.day03 import solution
+from aoc2024.util import get_input
+
+
+input_data = get_input("tests/testinput.day03")
+
+
+def test_solve_part1():
+    expected = None
+    actual = solution.solve_part1(input_data)
+    assert expected == actual
+
+
+def test_solve_part2():
+    expected = None
+    actual = solution.solve_part2(input_data)
+    assert expected == actual
