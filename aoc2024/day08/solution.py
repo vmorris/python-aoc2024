39a7bfde51454238a9059932b25d0997478fcfcd
Vance Morris <vmorris@us.ibm.com>
2024-12-07 18:47:05 -0600

    prep for day 8

---
+++ aoc2024/day08/solution.py
@@ -0,0 +1,19 @@
+import logging
+from aoc2024.util import get_input
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def solve_part1(entries):
+    return
+
+
+def solve_part2(entries):
+    return
+
+
+if __name__ == "__main__":  # pragma: no cover
+    entries = get_input("aoc2024/day08/input")
+    print(solve_part1(entries))
+    print(solve_part2(entries))
